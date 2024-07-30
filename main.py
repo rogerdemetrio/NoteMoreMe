b46aec42395db66939e77151c63f43f9bbf05871
Roger Demetrio <rogerdemetrio@live.com>
2024-07-30 06:11:05 -0300

    Alterado

--- main.py
+++ main.py
@@ -17,20 +17,21 @@ def main(page: ft.Page):
             content=ft.TextField(
                 autocorrect = True,
                 min_lines = 5,
-                max_lines = 25,
+                max_lines = 29,
                 multiline = True,
                 border = ft.InputBorder.NONE,
                 bgcolor = ft.colors.TRANSPARENT,
                 filled = True,
                 color = ft.colors.BLUE_GREY_800,
-                content_padding = 60
+                content_padding = ft.padding.symmetric(horizontal=62,vertical=48),
             ))
     
     cp = cv.Canvas(shapes=[cv.Path([cv.Path.MoveTo(60, 0),cv.Path.LineTo(60, 3000)],paint=ft.Paint(style=ft.PaintingStyle.STROKE,color=ft.colors.PINK_ACCENT_700),),],width=float("inf"))
-    for x in range(50):
-        y = x * 24
+    for x in range(33):
+        y = (x * 24) + 1
         linha = cv.Path([cv.Path.MoveTo(0, y),cv.Path.LineTo(3000, y)],paint=ft.Paint(style=ft.PaintingStyle.STROKE,color=ft.colors.BLUE_500),)
-        cp.shapes.append(linha)
+        if y > 48:
+            cp.shapes.append(linha)
     page.add(cp)
     page.add(input_texto)
     
