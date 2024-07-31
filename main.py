6c3c4acb9f97519a3e6ec16fc700a739ee1ddc58
Roger Demetrio <rogerdemetrio@live.com>
2024-07-31 02:11:03 -0300

    Adicionado o canvas responsivo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flet as ft
 import flet.canvas as cv
 
+
 def main(page: ft.Page):
     # Padrões da pagina
     page.window.height = 900
@@ -10,29 +11,26 @@ def main(page: ft.Page):
     page.padding = 0
     page.bgcolor = ft.colors.WHITE70
 
+    tamanho_letra = 16
+
     page.floating_action_button = ft.FloatingActionButton(icon=ft.icons.ADD)
 
-    input_texto = ft.Container(
-            expand=True,
-            content=ft.TextField(
-                autocorrect = True,
-                min_lines = 5,
-                max_lines = 29,
-                multiline = True,
-                border = ft.InputBorder.NONE,
-                bgcolor = ft.colors.TRANSPARENT,
-                filled = True,
-                color = ft.colors.BLUE_GREY_800,
-                content_padding = ft.padding.symmetric(horizontal=62,vertical=48),
-            ))
+    input_texto = ft.Container(expand=True,content=ft.TextField(autocorrect = True,min_lines = 5,max_lines = 29,multiline = True,border = ft.InputBorder.NONE,bgcolor = ft.colors.TRANSPARENT,
+            color = ft.colors.BLUE_GREY_800,content_padding = ft.padding.symmetric(horizontal=62,vertical=48),text_size= tamanho_letra
+        ))
+
+    def carrega_pagina(e):
+        page.controls.clear()
+        cp = cv.Canvas(shapes=[cv.Path([cv.Path.MoveTo(60, 0),cv.Path.LineTo(60, page.window.height)],paint=ft.Paint(style=ft.PaintingStyle.STROKE,color=ft.colors.PINK_ACCENT_700))])
+        for x in range(330):
+            y = (x * 24) + 1
+            linha = cv.Path([cv.Path.MoveTo(0, y),cv.Path.LineTo((page.window.width), y)],paint=ft.Paint(style=ft.PaintingStyle.STROKE,color=ft.colors.BLUE_400))
+            if y > 48 and y < (page.window.height - 60):
+                cp.shapes.append(linha)
+        page.add(cp)
+        page.add(input_texto)
     
-    cp = cv.Canvas(shapes=[cv.Path([cv.Path.MoveTo(60, 0),cv.Path.LineTo(60, 3000)],paint=ft.Paint(style=ft.PaintingStyle.STROKE,color=ft.colors.PINK_ACCENT_700),),],width=float("inf"))
-    for x in range(33):
-        y = (x * 24) + 1
-        linha = cv.Path([cv.Path.MoveTo(0, y),cv.Path.LineTo(3000, y)],paint=ft.Paint(style=ft.PaintingStyle.STROKE,color=ft.colors.BLUE_500),)
-        if y > 48:
-            cp.shapes.append(linha)
-    page.add(cp)
-    page.add(input_texto)
-    
+    page.on_resized = carrega_pagina
+    page.update()
+
 ft.app(target=main,name="NoteMoreMe") 
